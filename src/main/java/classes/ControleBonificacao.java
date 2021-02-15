cb4d87f7910fe11757c35823e786d2d2b8ba7198
Giulia Ferreira <giuliaferreiragomes36@gmail.com>
2021-02-15 16:40:24 -0300

    Criação classe para controlar as bonificações dos funcionários.

---
+++ src/main/java/classes/ControleBonificacao.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class ControleBonificacao {
+
+    private double soma;
+
+    //Pega a bonificação de um funcionário e coloca dentro da variável soma.
+    public void contador (Funcionario funcionario){
+        this.soma += funcionario.getBonificacao();
+    }
+
+    public double getSoma(){
+        return this.soma;
+    }
+
+}
