c5dc2721b75657a23e11112ca7dcaf31ec1f897f
Giulia Ferreira <giuliaferreiragomes36@gmail.com>
2021-02-15 16:41:26 -0300

    Teste com o método de controle de bonificação para observar o polimorfismo.

--- src/test/java/testes/TesteReferencias.java
+++ src/test/java/testes/TesteReferencias.java
@@ -1,5 +1,7 @@
 package testes;
 
+import classes.ControleBonificacao;
+import classes.EditorVideo;
 import classes.Funcionario;
 import classes.Gerente;
 
@@ -14,6 +16,31 @@ public class TesteReferencias {
         System.out.println(gerente.getNome());
 
         //gerente.autentica(123); -> não funciona porque funcionário não tem o método autentica.
+
+        //Polimorfismo entre funcionários:
+
+        Funcionario f1 = new Funcionario();
+        f1.setNome("Giulia");
+        f1.setSalario(1000);
+
+        Gerente g1 = new Gerente();
+        g1.setNome("Luana");
+        g1.setSalario(5000);
+
+        EditorVideo ev1 = new EditorVideo();
+        ev1.setNome("Rodes");
+        ev1.setSalario(2500);
+
+        ControleBonificacao controleBonificacao = new ControleBonificacao();
+        controleBonificacao.contador(f1);
+        controleBonificacao.contador(g1);
+        controleBonificacao.contador(ev1);
+
+        System.out.println(controleBonificacao.getSoma());
+
+
+
+
 
     }
 }
