f6d8cccc744ce9947fbefde2dde70d64d5e35bb6
Giulia Ferreira <giuliaferreiragomes36@gmail.com>
2021-02-15 16:40:01 -0300

    Criação classe "EditorVideo" com método para bonificação.

---
+++ src/main/java/classes/EditorVideo.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class EditorVideo extends Funcionario{
+
+    public double getBonificacao() {
+        System.out.println("Bonificação do EDITOR DE VIDEO");
+        return super.getBonificacao() + 100;
+    }
+}
