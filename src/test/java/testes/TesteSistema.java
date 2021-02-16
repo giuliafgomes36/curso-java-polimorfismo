8d6ccdd26b2e8a30ff11eca68bc0f4b1f7a1f435
Giulia Ferreira <giuliaferreiragomes36@gmail.com>
2021-02-16 14:32:14 -0300

    Teste da interface.

--- src/test/java/testes/TesteSistema.java
+++ src/test/java/testes/TesteSistema.java
@@ -1,6 +1,8 @@
 package testes;
 
+import contas.Cliente;
 import funcionarios.Administrador;
+import funcionarios.Autenticavel;
 import funcionarios.Gerente;
 import sistemas.SistemaInterno;
 
@@ -13,8 +15,12 @@ public class TesteSistema {
         Administrador administrador = new Administrador();
         administrador.setSenha(3333);
 
+        Autenticavel referencia = new Cliente();
+        referencia.setSenha(4444);
+
         SistemaInterno sistemaInterno = new SistemaInterno();
         sistemaInterno.autentica(gerente);
         sistemaInterno.autentica(administrador);
+        sistemaInterno.autentica(referencia);
     }
 }
