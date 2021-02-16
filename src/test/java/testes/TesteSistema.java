f1ebf7aabb41fdb0181b95b7554aa7c78104b8d8
Giulia Ferreira <giuliaferreiragomes36@gmail.com>
2021-02-16 13:38:16 -0300

    Testes no sistema de autenticação de senhas.

---
+++ src/test/java/testes/TesteSistema.java
@@ -0,0 +1,20 @@
+package testes;
+
+import funcionarios.Administrador;
+import funcionarios.Gerente;
+import funcionarios.SistemaInterno;
+
+public class TesteSistema {
+    public static void main(String[] args) {
+
+        Gerente gerente = new Gerente();
+        gerente.setSenha(2222);
+
+        Administrador administrador = new Administrador();
+        administrador.setSenha(3333);
+
+        SistemaInterno sistemaInterno = new SistemaInterno();
+        sistemaInterno.autentica(gerente);
+        sistemaInterno.autentica(administrador);
+    }
+}
