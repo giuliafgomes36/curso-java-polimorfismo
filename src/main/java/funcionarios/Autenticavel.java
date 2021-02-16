d0203bda8a4024fd5885e1f8b258e97b6179fbc8
Giulia Ferreira <giuliaferreiragomes36@gmail.com>
2021-02-16 14:30:43 -0300

    Transforma em interface.

---
+++ src/main/java/funcionarios/Autenticavel.java
@@ -0,0 +1,11 @@
+package funcionarios;
+
+//Interface = contrato
+//Quem assinar esse contrato precisa implementar: metodo setSenha e metodo autentica
+
+public interface Autenticavel {
+
+    void setSenha(int senha);
+
+    boolean autentica(int senha);
+}
