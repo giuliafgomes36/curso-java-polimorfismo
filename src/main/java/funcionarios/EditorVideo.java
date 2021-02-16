f92467993729d00b68349c83db5faacdd2425485
Giulia Ferreira <giuliaferreiragomes36@gmail.com>
2021-02-16 10:36:14 -0300

    Adição da notação @Override no método "getBonificação()".

--- src/main/java/funcionarios/EditorVideo.java
+++ src/main/java/funcionarios/EditorVideo.java
@@ -2,6 +2,7 @@ package funcionarios;
 
 public class EditorVideo extends Funcionario{
 
+    @Override
     public double getBonificacao() {
         System.out.println("Bonificação do EDITOR DE VIDEO");
         return super.getBonificacao() + 100;
--- src/main/java/funcionarios/Gerente.java
+++ src/main/java/funcionarios/Gerente.java
@@ -5,13 +5,14 @@ public class Gerente extends Funcionario{
 
     private int senha;
 
-    public void setSenha(int senha){
-        this.senha = senha;
-    }
-
+    @Override
     public double getBonificacao(){
         System.out.println("Bonificação do GERENTE");
         return super.getBonificacao() + super.getSalario();//o this geralmente é usado quando o atibuto é definido na classe.
+    }
+
+    public void setSenha(int senha){
+        this.senha = senha;
     }
 
     public boolean autentica (int senha){
